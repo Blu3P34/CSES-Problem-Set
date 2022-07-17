93d05a79605f359ac6046ffaf717a92e3c770f54
DaoMinhDuy <daominhduy2004@gmail.com>
2022-07-17 15:33:02 +0700

    Some stuffs

--- 1 Weird Algorithm.py
+++ 1 Weird Algorithm.py
@@ -1,8 +1,6 @@
 n = int(input())
 while n > 1 :
     print(n,end = " ")
-    if n % 2 == 0 :
-        n = n//2
-    else :
-        n = n*3 + 1
+    n = n*3 + 1 if n%2 == 1 else n//2
 print(1)
+
--- 2 Missing number.py
+++ 2 Missing number.py
@@ -1,10 +1,9 @@
 n = int(input())
-a = n*(n + 1)//2
 b = 0
 list = map(int, input().split())
 for i in list :
     b = b + i
-print(a-b)
+print(n*(n + 1)//2-b)
 
 
 
